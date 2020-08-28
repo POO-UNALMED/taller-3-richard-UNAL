6d82a0cbb13061b315fcf67f6d6e56289a5cbf18
Richard Montoya <richard.a.montoya.l@gmail.com>
2020-08-28 14:10:48 -0500

    Implement class Marca

--- src/main/java/taller3/televisores/Marca.java
+++ src/main/java/taller3/televisores/Marca.java
@@ -1,5 +1,24 @@
 package taller3.televisores;
 
 class Marca {
+	
+	private String nombre;
+	
+	
+	Marca(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	String getNombre() {
+		return nombre;
+	}
+
+
+	void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
 
 }
